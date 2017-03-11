c08b9883b93aa62691b87587802a5ffd74cbc6a5
Ed Toon <edtoon@edtoon.com>
2017-03-11 07:09:37 -0800

    Fix data dir and always run create/grant

--- bin/run_mysql.sh
+++ bin/run_mysql.sh
@@ -15,18 +15,18 @@ then
 
     sudo docker build -t merchantgg/mysql "${BIN_DIR}/../docker/mysql"
 
-    sudo mkdir -p "${DATA_DIR}"
+    sudo mkdir -p "${DATA_DIR}"/mysql
 
-    sudo docker run -d --name "${DB_DOCKER}" -v "${DATA_DIR}:/var/lib/mysql" -v "${DATA_DIR}:/home/mysql" \
+    sudo docker run -d --name "${DB_DOCKER}" -v "${DATA_DIR}/mysql:/var/lib/mysql" -v "${DATA_DIR}/mysql:/home/mysql" \
         -e MYSQL_ROOT_PASSWORD="${DB_ROOT_PASSWORD}" merchantgg/mysql
 
     until "${BASE_DIR}/bin/check_mysql.sh"
     do
         sleep 1
     done
-
-    echo "CREATE DATABASE IF NOT EXISTS \`${DB_DATABASE}\` CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin" | \
-        sudo docker exec -i "${DB_DOCKER}" mysql -uroot -p"${DB_ROOT_PASSWORD}"
-    echo "GRANT ALL ON \`${DB_DATABASE}\`.* TO '${DB_USERNAME}'@'%' IDENTIFIED BY '${DB_PASSWORD}'" | \
-        sudo docker exec -i "${DB_DOCKER}" mysql -uroot -p"${DB_ROOT_PASSWORD}"
 fi
+
+echo "CREATE DATABASE IF NOT EXISTS \`${DB_DATABASE}\` CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin" | \
+    sudo docker exec -i "${DB_DOCKER}" mysql -uroot -p"${DB_ROOT_PASSWORD}"
+echo "GRANT ALL ON \`${DB_DATABASE}\`.* TO '${DB_USERNAME}'@'%' IDENTIFIED BY '${DB_PASSWORD}'" | \
+    sudo docker exec -i "${DB_DOCKER}" mysql -uroot -p"${DB_ROOT_PASSWORD}"
