c9889bd8dc20d1e875931d471129b1bc743757db
Ed Toon <edtoon@edtoon.com>
2017-03-23 01:46:13 -0700

    more stuff that shouldn't be rsync'd. (not that anything should...)

--- bin/sync_ec2.sh
+++ bin/sync_ec2.sh
@@ -14,4 +14,6 @@ TARGET_DIR="${TARGET_DIR:=/mnt/gg/merchant/${APP_NAME}/src/current}"
 
 sudo chown -R "${USER}" "${SRC_DIR}"
 ssh "${SITE}" -- sudo chown -R ubuntu:ubuntu "${TARGET_DIR}"
-rsync -Pav -e 'ssh -C' --exclude 'apps' --exclude 'node_modules' "${SRC_DIR}/" "${SITE}:${TARGET_DIR}"
+rsync -Pav -e 'ssh -C' \
+  --exclude 'apps' --exclude 'dist' --exclude 'node_modules' --exclude 'yarn.lock' --exclude '.next' \
+  "${SRC_DIR}/" "${SITE}:${TARGET_DIR}"
