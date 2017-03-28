dfab91a1ef7af7c31c1baffe18d3d77a7078935d
Ed Toon <edtoon@edtoon.com>
2017-03-27 22:55:01 -0700

    stupid local cache

--- common/src/utils/lang.js
+++ common/src/utils/lang.js
@@ -1,3 +1,4 @@
+export const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
 export const unixTimestamp = () => (new Date().getTime() / 1000)
 export const valueUnlessUndef = (value, undefValue = null) => (value === undefined ? undefValue : value)
 export const valueOrDefault = (value, defaultValue = null) => ((value === undefined || value === null) ? defaultValue : value)
