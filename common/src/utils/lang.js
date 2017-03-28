c787c431a9353eeb3fb16629c2270e1e3aa0a386
Ed Toon <edtoon@edtoon.com>
2017-03-27 22:40:06 -0700

    simple util + yarn upgrade

--- common/src/utils/lang.js
+++ common/src/utils/lang.js
@@ -1,1 +1,3 @@
+export const unixTimestamp = () => (new Date().getTime() / 1000)
 export const valueUnlessUndef = (value, undefValue = null) => (value === undefined ? undefValue : value)
+export const valueOrDefault = (value, defaultValue = null) => ((value === undefined || value === null) ? defaultValue : value)
