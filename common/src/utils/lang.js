ae9ab99b777d2add9f5bb53ec97ffaeb199d315a
Ed Toon <edtoon@edtoon.com>
2017-04-03 11:21:47 -0700

    Remove microseconds from unixTimestamp()

--- common/src/utils/lang.js
+++ common/src/utils/lang.js
@@ -1,4 +1,4 @@
 export const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
-export const unixTimestamp = () => (new Date().getTime() / 1000)
+export const unixTimestamp = () => ((Date.now() / 1000) | 0)
 export const valueUnlessUndef = (value, undefValue = null) => (value === undefined ? undefValue : value)
 export const valueOrDefault = (value, defaultValue = null) => ((value === undefined || value === null) ? defaultValue : value)
