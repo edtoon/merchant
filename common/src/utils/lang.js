68d8b34170126147686d20ef9dac5d2d3c9e0be0
Ed Toon <edtoon@edtoon.com>
2017-04-06 07:27:27 -0700

    added randomInt method to common

--- common/src/utils/lang.js
+++ common/src/utils/lang.js
@@ -2,3 +2,4 @@ export const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
 export const unixTimestamp = () => ((Date.now() / 1000) | 0)
 export const valueUnlessUndef = (value, undefValue = null) => (value === undefined ? undefValue : value)
 export const valueOrDefault = (value, defaultValue = null) => ((value === undefined || value === null) ? defaultValue : value)
+export const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
