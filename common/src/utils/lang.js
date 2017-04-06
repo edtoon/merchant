e6b45f7b267d5bdacb7710c7f71490ce748eec71
Ed Toon <edtoon@edtoon.com>
2017-04-06 07:32:58 -0700

    nit

--- common/src/utils/lang.js
+++ common/src/utils/lang.js
@@ -1,5 +1,5 @@
+export const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
 export const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
 export const unixTimestamp = () => ((Date.now() / 1000) | 0)
 export const valueUnlessUndef = (value, undefValue = null) => (value === undefined ? undefValue : value)
 export const valueOrDefault = (value, defaultValue = null) => ((value === undefined || value === null) ? defaultValue : value)
-export const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
